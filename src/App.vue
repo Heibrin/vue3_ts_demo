ff9d0cb5f7052edfa4679c495791ebc3de7965e5
Heibrin <liqing0715@163.com>
2022-08-21 16:37:33 +0800

    🎉 init(init):

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+     <router-link to="/login">Login</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+*{
+  margin:0;
+  padding:0;
+}
+html,body,#app{
+  margin:0;
+  padding:0;
+}
+</style>
